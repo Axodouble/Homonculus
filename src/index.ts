cf3e32d23d0520d25ce2010bc839ae02040ce652
Axodouble <git@axodouble.com>
2025-09-21 10:09:46 +0800

    Fixed spelling and added discord.js dependency

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { Client, IntentsBitField } from "discord.js";
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+  ],
+});
+
+client.once("ready", () => {});
